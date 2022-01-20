77eb8ae12794c977f6816cb1bf4c92a797632ba4
Nuzhat Tabassum Progga <nuzhatprogga76@gmail.com>
2022-01-20 13:58:26 +0600

    row

--- lib/main.dart
+++ lib/main.dart
@@ -19,34 +19,33 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("My App"),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          alignment: Alignment.center,
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              // ignore: prefer_const_literals_to_create_immutables
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.shade400,
-                    blurRadius: 5,
-                    offset: Offset(2.0, 5.0))
-              ],
-              color: Colors.amber,
-              gradient: LinearGradient(
-                colors: [Colors.red, Colors.yellow],
-              )),
-          child: Text(
-            "I am a box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+      body: Container(
+        color: Colors.black,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.orange,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.redAccent,
+            ),
+          ],
         ),
       ),
     );
